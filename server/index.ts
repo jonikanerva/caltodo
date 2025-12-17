17f91a92f537fdab36a160dbcff3c287016ae86c
Joni Kanerva <jhonny@jhonny.org>
2025-12-17 20:21:46 +0200

    Handle errors without crashing the server

--- server/index.ts
+++ server/index.ts
@@ -55,7 +55,9 @@ app.use((req, res, next) => {
     const message = err.message || "Internal Server Error";
 
     res.status(status).json({ message });
-    throw err;
+    if (status >= 500) {
+      console.error(err);
+    }
   });
 
   // importantly only setup vite in development and after
